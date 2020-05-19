b744f069c50f759d10b4e137ed8f12a2754534a2
maximilian.anzinger <azure-pipeline-project-space-provisioner@azure-devops.com>
2020-05-19 14:34:01 +0200

    Add Function to remove Empty Txt files

--- RemoveImagesWithoutLabel.ps1
+++ RemoveImagesWithoutLabel.ps1
@@ -14,7 +14,11 @@ function Remove-Images {
     $images = gci $pathToImages | ? {$_.Extension -eq '.jpg'}
     $images | ForEach-Object {
         $labelPath = $_.FullName.Substring(0,$_.FullName.LastIndexOf('.')[0]) + ".txt"
-        if(!(Test-Path $labelPath)) {
+        if(Test-Path $labelPath) {
+            if((Get-Content $labelPath) -eq $null) {
+                Remove-Item $labelPath -Force
+            }
+        }else {
             Move-Item -Path $_.FullName -Destination $NoLabelPath -Force
         }
     }
