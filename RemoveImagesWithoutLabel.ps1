27885ee6ce82f103f4fffc3a26bf487dd4f97537
maximilian.anzinger <azure-pipeline-project-space-provisioner@azure-devops.com>
2020-05-19 14:48:49 +0200

    Add Progress Bar and User Information

--- RemoveImagesWithoutLabel.ps1
+++ RemoveImagesWithoutLabel.ps1
@@ -12,15 +12,23 @@ function Check-Folder() {
     
 function Remove-Images {
     $images = gci $pathToImages | ? {$_.Extension -eq '.jpg'}
+    $counter = 1
     $images | ForEach-Object {
         $labelPath = $_.FullName.Substring(0,$_.FullName.LastIndexOf('.')[0]) + ".txt"
+        $complete = $counter / $images.count * 100
+        Write-Progress -Activity "Searching for Images without Label" -Status "Running" -PercentComplete $complete
         if(Test-Path $labelPath) {
             if((Get-Content $labelPath) -eq $null) {
                 Remove-Item $labelPath -Force
+                Write-Host "Removing Label $($labelPath.Split($labelPath.LastIndexOf('\'),$labelPath.Length-1))" -ForegroundColor Yellow
+                Move-Item -Path $_.FullName -Destination $NoLabelPath -Force
+                Write-Host "Moving Image $($_.Name)" -ForegroundColor Yellow
             }
         }else {
             Move-Item -Path $_.FullName -Destination $NoLabelPath -Force
+            Write-Host "Moving Image $($NoLabelPath.Split($NoLabelPath.LastIndexOf('\\'),$NoLabelPath.Length-1))" -ForegroundColor Yellow
         }
+        $counter++
     }
 }
 
