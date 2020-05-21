c17932c85bc81ad0810d414064c1ed8f2cd03b40
Michael Mican <michael.mican@stud.th-deg.de>
2020-05-21 12:02:02 +0200

    add label file creation

--- TrainingsDataAccuireing/TrainingsDataLabelHelper.py
+++ TrainingsDataAccuireing/TrainingsDataLabelHelper.py
@@ -3,6 +3,7 @@ from darknetpy.detector import Detector
 import os
 import keyboard
 from matplotlib import image, patches, pyplot as plt
+from PIL import Image
 from enum import Enum
 
 class ImageClass(Enum):
@@ -20,6 +21,7 @@ imgFolderPath = scriptPath + "imgFolder/"
 correctlyLabledFolderPath = scriptPath + "correctLabled/"
 wronglyLabledFolderPath = scriptPath + "wrongLabled/"
 excludedImagesFolderPath = scriptPath + "excludedImages/"
+labelsFolderPath = scriptPath + "labels/"
 
 def main():
     createDir(imgFolderPath)
@@ -50,17 +52,38 @@ def getBoundingBoxes(imgPath):
 
 def evaluateImage(imgPath):
     bboxes = getBoundingBoxes(imgPath)
+    yoloFileStringContent = []
     fig, ax = plt.subplots(1)
     ax.imshow(image.imread(imgPath))
 
+
     for i, bbox in enumerate(bboxes):
         color = 'b'
+        classId = -1
         if bbox['class'] == "TL_R":
             color = 'r'
+            classId = 0
         if bbox['class'] == "TL_Y":
             color = 'y'
+            classId = 1
         if bbox['class'] == "TL_G":
             color = 'g'
+            classId = 2
+
+
+        if classId >= 0:
+
+            widthOfImage, heightOfImage = Image.open(imgPath).size
+
+            width = (bbox['right'] - bbox['left'])
+            height = (bbox['bottom'] - bbox['top'])
+
+            relXMiddle = (0.5 * width + bbox['left']) / widthOfImage
+            relYMiddle = (0.5 * height + bbox['top']) / heightOfImage
+            relWidth = width / widthOfImage
+            relHeight = height / heightOfImage
+
+            yoloFileStringContent.append("{} {} {} {} {}".format(str(classId), relXMiddle, relYMiddle, relWidth, relHeight))
 
         rect = patches.Rectangle(
             (bbox['left'], bbox['top']),
@@ -71,7 +94,7 @@ def evaluateImage(imgPath):
             facecolor='none'
         )
 
-        ax.text(bbox['left'], bbox['top'], bbox['class'], fontsize=12, bbox={
+        ax.text(bbox['left'], bbox['top'], bbox['class'] + bbox['prob'], fontsize=12, bbox={
                 'facecolor': color, 'pad': 2, 'ec': color})
 
         ax.add_patch(rect)
@@ -96,6 +119,10 @@ def evaluateImage(imgPath):
             imgClass = ImageClass.EXCLUDE
             print("Whats that doing here? - Take a look on this instead")
             break
+
+    if imgClass == ImageClass.CORRECT:
+        with open(labelsFolderPath + imgPath.split(".")[-2] + ".txt", "w+") as f:
+            f.writelines(yoloFileStringContent)
 
     plt.clf()
     plt.close()
